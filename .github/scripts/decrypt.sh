2ef13a2f176626f5f7f52d9eb4e929872de27f9e
Sijan Rijal <47872289+sijanr@users.noreply.github.com>
2020-09-07 00:50:48 -0700

    Update decrypt.sh

--- .github/scripts/decrypt.sh
+++ .github/scripts/decrypt.sh
@@ -2,4 +2,4 @@
 
 # Decrypt the file
 gpg --quiet --batch --yes --pinentry loopback --decrypt --passphrase="$FIREBASE_SERVICE" \
---output $./app/google-services.json google-services.json.gpg
+--output ./app/google-services.json google-services.json.gpg
